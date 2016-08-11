4eece647d0e4b7918925828bd91cc02ea2287b2a
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-11 16:23:11 +0100

    games

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 </head>
 <body>
 <img src='ave.jpg'><br />
-Online playable versions of AVE are coming soon. For now, you can download
-AVE from <a href='https://github.com/mscroggs/AVE'>GitHub</a>.
+Online playable versions of AVE games are coming soon.
+For now, you can download AVE from
+<a href='https://github.com/mscroggs/AVE'>GitHub</a>.
 </body>
 </html>
