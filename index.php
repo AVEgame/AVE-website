2744e592d8f0c71f21600477fd2f5bcebe6bbac3
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-11 16:22:22 +0100

    Added git link

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <title>AVE: Adventure! Villainy! Excitement!</title>
 </head>
 <body>
-<img src='ave.jpg'>
+<img src='ave.jpg'><br />
+Online playable versions of AVE are coming soon. For now, you can download
+AVE from <a href='https://github.com/mscroggs/AVE'>GitHub</a>.
 </body>
 </html>
