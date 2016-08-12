054d6c4b3953be1acd19ff09c38f61a7f7c53bd5
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-12 16:04:42 +0100

    Styles for the game

--- index.php
+++ index.php
@@ -4,6 +4,20 @@ include("intro.php");
 Online playable versions of AVE games are coming soon.
 For now, you can download AVE from
 <a href='https://github.com/mscroggs/AVE'>GitHub</a>.
+
+<script type='text/javacript' src='ave/game.js'></script>
+<div class='game'>
+<div class='gameend'>GAME OVER<br /></br />
+<div class='menuitem'>Play again</div>
+<div class='menuitem'>Play a different game</div>
+</div>
+<div class='roominfo'>You wake up. What do you want to do?</div>
+<div class='inventory'>INVENTORY<br />Hat</div>
+<div class='menu'>
+<div class='menuitem' onclick='alert("up")'>Get up</div>
+</div>
+</div>
+
 <?php
 include("outro.php");
 ?>
