9282516f692365d338e79d5f336885f825e940bf
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-14 18:44:45 +0100

    Added instructions to readme on how to build

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 AVE: Adventure! Villainy! Excitement! is a text-based game engine
 written by Matthew Scroggs.
 
-This repository is for its website.
+This repository is for its website. To build the website, build the javascript version of the 
+[AVE repository](http://github.com/mscroggs/AVE) and place it in the /ave folder of this repository.
 
 You can find the AVE repository at [github.com/mscroggs/AVE](http://github.com/mscroggs/AVE).
--- index.php
+++ index.php
@@ -5,16 +5,18 @@ Online playable versions of AVE games are coming soon.
 For now, you can download AVE from
 <a href='https://github.com/mscroggs/AVE'>GitHub</a>.
 
-<script type='text/javacript' src='ave/game.js'></script>
+<script type='text/javascript' src='ave/game.js'></script>
 <div class='game'>
-<div class='gameend'>GAME OVER<br /></br />
-<div class='menuitem'>Play again</div>
-<div class='menuitem'>Play a different game</div>
+<div style='width:100%;text-align:right;margin-bottom:3px'><span style='color:red'>A</span><span style='color:green'>V</span><span style='color:blue'>E</span></div>
+<div id='gameend'>
+<div id='gameendtext'>GAME OVER</div>
+<div class='menuitem' onclick='gameRestart()'>Play again</div>
+<div class='menuitem' onclick='gameList()'>Play a different game</div>
 </div>
-<div class='roominfo'>You wake up. What do you want to do?</div>
-<div class='inventory'>INVENTORY<br />Hat</div>
-<div class='menu'>
-<div class='menuitem' onclick='alert("up")'>Get up</div>
+<div id='roominfo'>You wake up. What do you want to do?</div>
+<div id='inventory'>INVENTORY<br />Hat</div>
+<div id='menu'>
+<div class='menuitem' onclick='loadRoom("up")'>Get up</div>
 </div>
 </div>
 
