69bae81ac5c0367d635753be4f306a6e0db37512
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-14 19:28:52 +0100

    Added footer

--- outro.php
+++ outro.php
@@ -1,4 +1,5 @@
 <?php
 ?>
+<div class='foot'>&copy; M. Scroggs & G. Grasso 2016. If you find an error on this website, please submit a bug report <a href='https://github.com/mscroggs/AVE/issues'>here</a>.</div>
 </body>
 </html>
