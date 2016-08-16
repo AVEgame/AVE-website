5cce71757c45c0b346d490a94f3a92a2c5275c7f
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-16 14:09:27 +0100

    Links different colours on hover

--- outro.php
+++ outro.php
@@ -1,5 +1,14 @@
 <?php
 ?>
 <div class='foot'>&copy; M. Scroggs & G. Grasso 2016. If you find an error on this website, please submit a bug report <a href='https://github.com/mscroggs/AVE/issues' target='new'>here</a>.</div>
+
+<script type='text/javascript'>
+l = document.links;
+cols = Array("red","green","blue")
+for(var i=0; i<l.length; i++) {
+//  alert(l[i].href);
+    l[i].className = cols[i%3];
+}
+</script>
 </body>
 </html>
