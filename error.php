2326d262d033eebd5ea5541b087f1f997f451965
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-22 21:48:55 +0100

    </div>

--- error.php
+++ error.php
@@ -3,7 +3,7 @@ include("intro.php");
 ?>
 Error <?php echo($_GET['e']); ?>
 <br /><br />
-If you followed a link to get here, please submit a bug report <a href='https://github.com/mscroggs/AVE/issues' target='new'>here</a>.</div>
+If you followed a link to get here, please submit a bug report <a href='https://github.com/mscroggs/AVE/issues' target='new'>here</a>.
 <br /><br />
 You can now play AVE games online <a href='/play'>here</a>.
 <?php
