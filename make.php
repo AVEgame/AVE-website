3ba3eb8a9bde17d2e85dfb46ed4cd907e4659acc
MScroggs <matthew.w.scroggs@gmail.com>
2016-09-07 20:13:55 +0100

    link to js version

--- make.php
+++ make.php
@@ -4,10 +4,13 @@ include("intro.php");
 As well as a game engine, <?php echo ave();?> is also a simple language that allows
 you to write your own games.
 <br /><br />
-Before making your own games, you should download the Python version of <?php echo ave();?>.
+Before making your own games, you should download the latest version of <?php echo ave();?>.
 This will allow you to run your game to test it.
-The easiest way to do this is to
-<a href='https://github.com/AVEgame/AVE/releases/download/v1.3/AVE1.3-python.zip' target='new'>download the .zip of the latest version</a>.
+The easiest way to do this is to download the .zip of the latest version:
+<ul>
+<li><a href='https://github.com/AVEgame/AVE/releases/download/v1.3/AVE1.3-python.zip' target='new'>Python version (Linux/Mac)</a></li>
+<li><a href='https://github.com/AVEgame/AVE/releases/download/v1.3/AVE1.3-javascript.zip' target='new'>Python version (Windows)</a></li>
+</ul>
 <br /><br />
 Once you have downloaded the Python version of <?php echo ave();?>, you are ready to write your own game.
 To help you with this, you can use the following documentation:
