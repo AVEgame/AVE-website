6eb9114891d7a66899c4f3890c6e190bd90d31f3
MScroggs <matthew.w.scroggs@gmail.com>
2016-09-10 20:29:53 +0100

    Added virtualbox to docs

--- make.php
+++ make.php
@@ -9,6 +9,7 @@ This will allow you to run your game to test it.
 The easiest way to do this is to download the .zip of the latest version:
 <ul>
 <li><a href='https://github.com/AVEgame/AVE/releases/download/v1.3/AVE1.3-python.zip' target='new'>Python version (Linux/Mac)</a></li>
+<li><a href='/docs/virtualbox.md' target='new'>Running a VirtualBox image (All platforms)</a></li>
 </ul>
 <br /><br />
 Once you have downloaded the Python version of <?php echo ave();?>, you are ready to write your own game.
