49d8a721afbf61cf45df6d8efc323b2ac2dd7db5
MScroggs <matthew.w.scroggs@gmail.com>
2016-09-11 15:25:58 +0100

    not necessarily the python version

--- make.php
+++ make.php
@@ -11,8 +11,7 @@ The easiest way to do this is to download the .zip of the latest version:
 <li><a href='https://github.com/AVEgame/AVE/releases/download/v1.3/AVE1.3-python.zip' target='new'>Python version (Linux/Mac)</a></li>
 <li><a href='/docs/virtualbox.md' target='new'>Running a VirtualBox image (All platforms)</a></li>
 </ul>
-<br /><br />
-Once you have downloaded the Python version of <?php echo ave();?>, you are ready to write your own game.
+Once you have downloaded a version of <?php echo ave();?>, you are ready to write your own game.
 To help you with this, you can use the following documentation:
 <ul>
 <li><a href='/docs/guide.md'>How to make your own game</a></li>
